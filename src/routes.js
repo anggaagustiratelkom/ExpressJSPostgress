19559faf747505f011ec29c03a2fcf7aa4ca13f8
anggaagustiratelkom <anggaagustira@gmail.com>
2022-12-05 23:35:04 +0700

    tambahan server

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const client = require("./connection");
+// app.use(bodyParser.json());
+
+// app.get("/books", (req, res) => {
+//   client.query(`SELECT * FROM books`, (err, result) => {
+//     if (!err) {
+//       res.send(result);
+//     }
+//   });
+// });
